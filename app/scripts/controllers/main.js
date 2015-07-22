cded03c2659ca41c1029983f83891477131765de
Roy Hewitt <roy_hewitt@fws.gov>
2015-07-22 19:49:41 -0400

    Added logout function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,18 @@
  * Controller of the ideasApp
  */
 angular.module('ideasApp')
-  .controller('MainCtrl', function ($scope, $auth, $window) {
+  .controller('MainCtrl', function ($scope, $auth, $window, $state) {
 
     $scope.authenticate = function(provider) {
       $auth.authenticate(provider).then(function (response) {
         $window.localStorage.user = JSON.stringify(response.data.user);
       });
+    };
+
+    $scope.logOut = function () {
+      $auth.logout();
+      delete $window.localStorage.user;
+      $state.go('home');
     };
 
     $scope.isAuthenticated = function() {
