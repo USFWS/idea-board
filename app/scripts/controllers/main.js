0257eb2decfa0d7502e7c2e58fca3da400276096
Roy Hewitt <roy_hewitt@fws.gov>
2015-07-23 13:34:10 -0400

    Resize prof pic add to header

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,11 @@
 angular.module('ideasApp')
   .controller('MainCtrl', function ($scope, $auth, $window, $state, toastr, User) {
 
+    User.getOne(User.getId()).then(function (response) {
+      $scope.user = response.data;
+      $scope.user.headerpic = $scope.user.picture.replace('?sz=50', '?sz=40');
+    });
+
     $scope.authenticate = function(provider) {
       $auth.authenticate(provider).then(function (response) {
         var message = 'Welcome, ' + User.getUsername();
