8fe0e7e6a25d79a64aff3894c786e69924b696ad
Roy Hewitt <roy_hewitt@fws.gov>
2015-07-22 20:07:01 -0400

    Added profile route

---
+++ app/scripts/controllers/profile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ideasApp.controller:ProfileCtrl
+ * @description
+ * # ProfileCtrl
+ * Controller of the ideasApp
+ */
+angular.module('ideasApp')
+  .controller('ProfileCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
