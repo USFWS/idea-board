de3fa01b97bb3e0d712c91e1c3671a505b4e93c5
Roy Hewitt <roy_hewitt@fws.gov>
2015-07-23 13:32:01 -0400

    Added Idea service

---
+++ app/scripts/services/Idea.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ideasApp.Idea
+ * @description
+ * # Idea
+ * Service in the ideasApp.
+ */
+angular.module('ideasApp')
+  .service('Idea', function ($http, API_URL, User) {
+    function getAll() {
+      return $http.get(API_URL + 'idea');
+    }
+
+    function getOne(id) {
+      return $http.get(API_URL + 'idea/' + id);
+    }
+
+    function create(params) {
+      params.creator = User.getId();
+      console.log(params);
+      return $http.post(API_URL + 'idea', params);
+    }
+
+    return {
+      getAll: getAll,
+      getOne: getOne,
+      create: create
+    };
+  });
