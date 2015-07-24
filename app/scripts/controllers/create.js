31d3de03ff46cf3b011a3c1b4192731437fa8abd
Roy Hewitt <roy_hewitt@fws.gov>
2015-07-24 12:58:43 -0400

    Empty form after submission

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -13,8 +13,8 @@ angular.module('ideasApp')
 
     $scope.create = function() {
       Idea.create($scope.idea).then(function (response) {
-        console.log(response);
-        toastr.success('Created new Idea!');
+        toastr.success('Created new Idea!', response.statusText);
+        $scope.idea = {};
       }).catch(function (response) {
         toastr.error('Unable to create new Idea.', response.statusText);
       });
