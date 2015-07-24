7ba97cc0033cc80fa666d7c48406e3563ffb76f3
Roy Hewitt <roy_hewitt@fws.gov>
2015-07-24 15:31:58 -0400

    Update to use idea card directive

--- app/scripts/controllers/ideas.js
+++ app/scripts/controllers/ideas.js
@@ -8,45 +8,7 @@
  * Controller of the ideasApp
  */
 angular.module('ideasApp')
-  .controller('IdeasCtrl', function ($scope, ideas, Idea, User, toastr, $location) {
+  .controller('IdeasCtrl', function ($scope, ideas, $location) {
     $scope.ideas = ideas.data;
     $scope.location = $location.$$absUrl;
-
-    $scope.hasVoted = function(vote) {
-      if (!vote) return;
-      var voted;
-      _.each(vote.score, function(el) {
-        voted = (el.id === User.getId()) ? true : false;
-      });
-      return voted;
-    };
-
-    $scope.submitVote = function(voteId) {
-      var url = buildUrl(voteId);
-      User.submitVote(url).then(function () {
-        Idea.getAll().then(function (response) {
-          $scope.ideas = response.data;
-          $scope.hasVoted();
-        });
-      }).catch(function (response) {
-        toastr.error('Vote Unsuccessful', response.statusText);
-      });
-    };
-
-    $scope.removeVote = function(voteId) {
-      var url = buildUrl(voteId);
-      User.removeVote(url).then(function () {
-        Idea.getAll().then(function (response) {
-          $scope.ideas = response.data;
-          $scope.hasVoted();
-        });
-      }).catch(function (response) {
-        toastr.error('Could not remove your vote.', response.statusText);
-      });
-    };
-
-    function buildUrl(voteId) {
-      var id = User.getId();
-      return 'user/' + id + '/votes/' + voteId;
-    }
   });
