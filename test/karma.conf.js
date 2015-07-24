b5b6c314a81dd23fcf8855a7832d81bb2dcdd213
Roy Hewitt <roy_hewitt@fws.gov>
2015-07-24 10:01:18 -0400

    Added underscore

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
       'bower_components/satellizer/satellizer.js',
       'bower_components/angular-toastr/dist/angular-toastr.tpls.js',
       'bower_components/ng-tags-input/ng-tags-input.min.js',
+      'bower_components/underscore/underscore.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
