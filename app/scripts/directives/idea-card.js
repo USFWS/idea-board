d98da1c03dfa6f59f01cd35b0f2229caef2e79c4
Roy Hewitt <roy_hewitt@fws.gov>
2015-07-24 15:30:38 -0400

    Created idea card component directive

---
+++ app/scripts/directives/idea-card.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name ideasApp.directive:ideaCard
+ * @description
+ * # ideaCard
+ */
+angular.module('ideasApp')
+  .directive('ideaCard', function () {
+    return {
+      templateUrl: 'views/partials/idea.html',
+      restrict: 'E',
+      scope: {
+        idea: '=',
+        user: '='
+      },
+      controller: function ($scope, User, Idea, toastr) {
+
+        $scope.hasVoted = function() {
+          var voted;
+          _.each($scope.idea.score, function(el) {
+            voted = (el.id === User.getId()) ? true : false;
+          });
+          return voted;
+        };
+
+        $scope.submitVote = function(ideaId) {
+          var url = buildUrl(ideaId);
+          User.submitVote(url).then(function () {
+            Idea.getOne(ideaId).then(function (response) {
+              $scope.idea = response.data;
+            });
+          }).catch(function (response) {
+            toastr.error('Vote Unsuccessful', response.statusText);
+          });
+        };
+
+        $scope.removeVote = function(ideaId) {
+          var url = buildUrl(ideaId);
+          User.removeVote(url).then(function () {
+            Idea.getOne(ideaId).then(function (response) {
+              $scope.idea = response.data;
+            });
+          }).catch(function (response) {
+            toastr.error('Could not remove your vote.', response.statusText);
+          });
+        };
+
+        function buildUrl(ideaId) {
+          var id = User.getId();
+          return 'user/' + id + '/votes/' + ideaId;
+        }
+      }
+    };
+  });
