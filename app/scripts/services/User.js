23771116a69b6883a10c2dacf19780ed3bdc1784
Roy Hewitt <roy_hewitt@fws.gov>
2015-07-29 14:43:49 -0400

    Add update function for profile

--- app/scripts/services/User.js
+++ app/scripts/services/User.js
@@ -11,6 +11,10 @@ angular.module('ideasApp')
   .service('User', function ($http, $auth, API_URL) {
     var payload = $auth.getPayload();
 
+    function isAdmin() {
+      return  payload.act === 'Admin';
+    }
+
     function getAll() {
       return $http.get(API_URL + 'user');
     }
@@ -35,12 +39,18 @@ angular.module('ideasApp')
       return $http.delete(API_URL + url);
     }
 
+    function update(profileData) {
+      return $http.put(API_URL + 'user/' + getId(), profileData);
+    }
+
     return {
+      isAdmin: isAdmin,
       getAll: getAll,
       getOne: getOne,
       getUsername: getUsername,
       getId: getId,
       submitVote: submitVote,
-      removeVote: removeVote
+      removeVote: removeVote,
+      update: update
     };
   });
