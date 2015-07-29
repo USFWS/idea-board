d2710165bb7f3da5b0e114224004d4f88ffd2722
Roy Hewitt <roy_hewitt@fws.gov>
2015-07-29 19:34:42 -0400

    Added get functions to services

--- app/scripts/services/attachment.js
+++ app/scripts/services/attachment.js
@@ -8,6 +8,18 @@
  * Service in the ideasApp.
  */
 angular.module('ideasApp')
-  .service('Attachment', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('Attachment', function ($http, API_URL) {
+    var getAll = function(query) {
+      var url = (query) ? API_URL + 'attachment/' + query : API_URL + 'attachment';
+      return $http.get(url);
+    };
+
+    var getOne = function(id) {
+      return $http.get(API_URL + 'attachment/' + id);
+    };
+
+    return {
+      getAll: getAll,
+      getOne: getOne
+    };
   });
--- app/scripts/services/comment.js
+++ app/scripts/services/comment.js
@@ -8,6 +8,18 @@
  * Service in the ideasApp.
  */
 angular.module('ideasApp')
-  .service('Comment', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('Comment', function (API_URL, $http) {
+    var getAll = function(query) {
+      var url = (query) ? API_URL + 'comment' + query : API_URL + 'comment';
+      return $http.get(url);
+    };
+
+    var getOne = function(id) {
+      return $http.get(API_URL + 'comment/' + id);
+    };
+
+    return {
+      getAll: getAll,
+      getOne: getOne
+    };
   });
