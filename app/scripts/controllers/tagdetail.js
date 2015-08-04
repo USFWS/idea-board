cf59d3212d0a9925edd2674bea0d2710f706b11c
Roy Hewitt <roy_hewitt@fws.gov>
2015-08-04 13:49:18 -0400

    Display all ideas that include tag on tag detail page

--- app/scripts/controllers/tagdetail.js
+++ app/scripts/controllers/tagdetail.js
@@ -8,8 +8,8 @@
  * Controller of the ideasApp
  */
 angular.module('ideasApp')
-  .controller('TagDetailCtrl', function ($scope, tag) {
+  .controller('TagDetailCtrl', function ($scope, tag, ideas, user) {
     $scope.tag = tag.data;
-    // $scope.ideas = ideas.data;
-    // $scope.userId = User.getId();
+    $scope.ideas = ideas.data;
+    $scope.user = user.data;
   });
