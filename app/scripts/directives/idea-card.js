2216fc32ef4ff5135e5ed0febb1464a14dd02dad
Roy Hewitt <roy_hewitt@fws.gov>
2015-08-12 14:31:14 -0400

    Use new toggle directives

--- app/scripts/directives/idea-card.js
+++ app/scripts/directives/idea-card.js
@@ -13,44 +13,8 @@ angular.module('ideasApp')
       restrict: 'E',
       scope: {
         idea: '=',
-        id: '@'
-      },
-      controller: function ($scope, User, Idea, toastr) {
-
-        $scope.hasVoted = function() {
-          var voted;
-          _.each($scope.idea.score, function(el) {
-            voted = (el.id === $scope.id) ? true : false;
-          });
-          return voted;
-        };
-
-        $scope.submitVote = function(ideaId) {
-          var url = buildUrl(ideaId);
-          User.submitVote(url).then(function () {
-            Idea.getOne(ideaId).then(function (response) {
-              $scope.idea = response.data;
-            });
-          }).catch(function (response) {
-            toastr.error('Vote Unsuccessful', response.statusText);
-          });
-        };
-
-        $scope.removeVote = function(ideaId) {
-          var url = buildUrl(ideaId);
-          User.removeVote(url).then(function () {
-            Idea.getOne(ideaId).then(function (response) {
-              $scope.idea = response.data;
-            });
-          }).catch(function (response) {
-            toastr.error('Could not remove your vote.', response.statusText);
-          });
-        };
-
-        function buildUrl(ideaId) {
-          var id = User.getId();
-          return 'user/' + id + '/votes/' + ideaId;
-        }
+        userId: '@',
+        detailed: '@'
       }
     };
   });
