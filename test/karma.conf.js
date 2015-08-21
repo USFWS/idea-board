8b090d7b4e53eabf16359f432431ce5498bb1519
Roy Hewitt <roy_hewitt@fws.gov>
2015-08-21 13:37:54 -0400

    Added angular sanitize

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,9 +31,10 @@ module.exports = function(config) {
       'bower_components/ui-router/release/angular-ui-router.js',
       'bower_components/satellizer/satellizer.js',
       'bower_components/angular-toastr/dist/angular-toastr.tpls.js',
-      'bower_components/ng-tags-input/ng-tags-input.min.js',
       'bower_components/underscore/underscore.js',
       'bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js',
+      'bower_components/angular-ui-select/dist/select.js',
+      'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
